14142a57812b7809cb37be2a8225841792d99eb7
ayushkumarrojha <ayushk@geekyants.com>
2022-06-13 12:41:27 +0530

    removed login() from reusable methods

--- src/test/java/Utilities/ReusableMethod.java
+++ src/test/java/Utilities/ReusableMethod.java
@@ -1,9 +1,5 @@
 package Utilities;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-
-import Payload.LoginReqBod;
 import io.restassured.path.json.JsonPath;
 
 public class ReusableMethod {
@@ -12,16 +8,4 @@ public class ReusableMethod {
 		return js1;
 	}
 
-
-	public static String LoginAsSuperAdmin() {
-		String response = given().log().all().header("Content-Type","application/json")
-				.body(LoginReqBod.loginBod("as", "bs")).when().post("/auth/login")
-				.then().log().all().assertThat().statusCode(200).body("success", equalTo(true))
-				.header("X-Powered-By", "Express").extract().response().asString();
-				
-				System.out.println(response);
-				JsonPath js = ReusableMethod.rawToJson(response);//for parsing json
-				String accToken = js.getString("data.authToken.accessToken");
-				return accToken;
-	}
 }
